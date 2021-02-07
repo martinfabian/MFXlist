955f49f37fa15ee6c88b6bfbc3f7ea450946934b
M Fabian <fabian@chalmers.se>
2021-02-07 20:58:06 +0100

    version 0.9.6

--- MFX-list.lua
+++ MFX-list.lua
@@ -1,6 +1,6 @@
 -- @description FX list for Reaper left docker (MFX-list)
 -- @author M Fabian, inlcudes code by Edgemeal
--- @SCRIPT_version 0.9.5
+-- @SCRIPT_version 0.9.6
 -- @changelog
 --   Nothing yet, or rather... everything
 -- @link
@@ -61,7 +61,7 @@ MFXlist =
   FONT_INVFLAG = 0x56000000,    -- invert  
   
   -- Script specific constants, from here below change only if you really know what you are doing
-  SCRIPT_VERSION = "v0.9.5",
+  SCRIPT_VERSION = "v0.9.6",
   SCRIPT_NAME = "MFX-list",
   SCRIPT_AUTHORS = {"M Fabian"},
   SCRIPT_YEAR = "2020-2021",
