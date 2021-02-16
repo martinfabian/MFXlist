3d2efbb88e3de1429436dd78516363eddc83be8d
M Fabian <fabian@chalmers.se>
2021-02-16 18:42:16 +0100

    Clean up some (remove commas etc)

--- MFXpreset.lua
+++ MFXpreset.lua
@@ -42,19 +42,19 @@ end
 -- MFXlist customizable settings, commented out ones are default values
 MFXlist.COLOR_EMPTYSLOT = {60/255, 0, 60/255}
 -- MFXlist.COLOR_FXHOVERED = {1, 1, 0}
--- MFXlist.COLOR_DROPMOVE = {0, 0, 1},
--- MFXlist.COLOR_DROPCOPY = {0, 1, 0},
+-- MFXlist.COLOR_DROPMOVE = {0, 0, 1}
+-- MFXlist.COLOR_DROPCOPY = {0, 1, 0}
 MFXlist.COLOR_SELECTEDTRACK = {1, 1, 0}
--- MFXlist.SHOW_FXTYPE = false, -- Show FX type prefix, JS:, VST:, VSTi:, VID:
+-- MFXlist.SHOW_FXTYPE = false -- Show FX type prefix, JS:, VST:, VSTi:, VID:
 -- MFXlist.MENU_QUICKFX = {"ReaEQ", "ReaComp", "ReaFIR", "ReaDelay"}
--- MFXlist.FX_DISABLEDA = 0.3, -- fade of name for disabled FX
--- MFXlist.FX_OFFLINEDA = 0.1, -- even fainter for offlined FX
--- MFXlist.FONT_NAME1 = "Arial",
--- MFXlist.FONT_NAME2 = "Courier New",
--- MFXlist.FONT_SIZE1 = 14,
--- MFXlist.FONT_SIZE2 = 16,
--- MFXlist.SLOT_HEIGHT = 13, -- pixels high
--- MFXlist.ALT_FXBROWSER = nil, -- Alternative FX browswer, put command ID here
+-- MFXlist.FX_DISABLEDA = 0.3 -- fade of name for disabled FX
+-- MFXlist.FX_OFFLINEDA = 0.1 -- even fainter for offlined FX
+-- MFXlist.FONT_NAME1 = "Arial"
+-- MFXlist.FONT_NAME2 = "Courier New"
+-- MFXlist.FONT_SIZE1 = 14
+-- MFXlist.FONT_SIZE2 = 16
+-- MFXlist.SLOT_HEIGHT = 13 -- pixels high
+-- MFXlist.ALT_FXBROWSER = nil -- Alternative FX browswer, put command ID here
 	-- These are the command IDs on my machine
     -- "_RS490460a16d7e7bb0285ccb1891b67f8f59593a61", -- Quick Adder
     -- "_RS36fe8a223d7ec08e45d4e8569c9bc15b9e417dfa", -- Fast FX finder
